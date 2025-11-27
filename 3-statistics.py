e7910fc4d4fbb4fb71b651c8183b0545ea45d272
ViitoDev <joaovictorpiresdev@gmail.com>
2025-11-27 20:04:15 -0300

    Fazendo estudo do módulo statistic

---
+++ 3-statistics.py
@@ -0,0 +1,14 @@
+import statistics
+
+#1- Aplicando a média
+print(statistics.mean([3, 2, 3, 8, 9]))
+
+#2- Mediana
+print(statistics.median([1, 2, 4, 8, 9]))
+print(statistics.median([1,2,3,7,8,9]))
+
+#3- Moda
+print(statistics.mode([2,5,3,2,8,8,3,9,4,2,5,6]))
+
+#4- Desvio padrão
+print(statistics.stdev([1, 1.5, 2, 2.5, 3, 3.5, 4, 4.5]))
