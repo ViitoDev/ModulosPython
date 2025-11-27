9bab98ae6c9e54ee486fee66ddd6d0833a7db6c7
ViitoDev <joaovictorpiresdev@gmail.com>
2025-11-27 18:17:13 -0300

    Fazendo estudo dos módulos

---
+++ main.py
@@ -0,0 +1,12 @@
+#Programa principal
+import math_operations
+from math_operations import multiply, divide
+import string_utils
+
+print(math_operations.sum(5,3))
+print(math_operations.substract(5,3))
+print(multiply(5,3))
+print(divide(5,3))
+print(string_utils.capitalize("Hello"))
+print(string_utils.reverse_string("Python"))
+print(string_utils.count("Apple"))
